744b64005f67b886818e2fd4319ae0f43503b5ab
吴金东 <wujindong@wujindongdeMacBook-Pro.local>
2017-09-05 18:28:13 +0800

    Origin

--- api_gateway/src/main/java/com/iot/nero/api_gateway/core/firewall/filter/CORSFilter.java
+++ api_gateway/src/main/java/com/iot/nero/api_gateway/core/firewall/filter/CORSFilter.java
@@ -1,12 +1,17 @@
 package com.iot.nero.api_gateway.core.firewall.filter;
 
 
+import com.iot.nero.utils.spring.PropertyPlaceholder;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Author nero
@@ -16,8 +21,23 @@ import java.io.IOException;
  */
 @Component
 public class CORSFilter implements Filter {
+    private static final String CROS_FILE_DIR = PropertyPlaceholder.getProperty("crosFilter.file").toString();
+    private Map<String,String> crosMap = new HashMap<String, String>();
     public void init(FilterConfig filterConfig) throws ServletException {
-
+            File f = new File(CROS_FILE_DIR);
+            BufferedReader br = null;
+            String line;
+            try {
+                br = new BufferedReader(new FileReader(f));
+                while ((line = br.readLine()) != null) {
+                    String key = line.split(":")[0];
+                    String value = line.split(":")[1];
+                    crosMap.put(key,value);
+                }
+                br.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
     }
 
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -25,16 +45,16 @@ public class CORSFilter implements Filter {
         String origin = (String) servletRequest.getRemoteHost()+":"+servletRequest.getRemotePort();
         HttpServletRequest request = (HttpServletRequest)servletRequest;
         if(request.getHeader("Origin")!=null) {
-            if (request.getHeader("Origin").contains("localhost")) {
-                response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-            }else if(request.getHeader("Origin").contains("47.94.251.146")){
-                response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-            }else if(request.getHeader("Origin").contains("cenocloud")){
-                response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-            }else if(request.getHeader("Origin").contains("47.94.46.29")){
-                response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+            Set set = crosMap.entrySet();
+            Iterator it = set.iterator();
+            while (it.hasNext()){
+                    Map.Entry entry = (Map.Entry)it.next();
+                if (request.getHeader("Origin").contains(entry.getValue().toString())) {
+                    response.setHeader("Access-Control-Allow-Origin",request.getHeader ("Origin"));
+                }
             }
         }
+
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
